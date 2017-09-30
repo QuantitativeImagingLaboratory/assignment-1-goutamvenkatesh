5e3f9e8fc26318337a840ad91520a53fcdc679fe
goutamvenkatesh <goutam.venkatesh@gmail.com>
2017-09-30 09:37:47 -0500

    update 30th september - threshold operation
    Added a method to perform thresholding

--- region_analysis/binary_image.py
+++ region_analysis/binary_image.py
@@ -26,9 +26,44 @@ class binary_image:
         returns: an optimal threshold value"""
 
         threshold = 0
+        
+        avg=len(hist)/2
+        overall = 0
 
+        for i in range(int(avg)):
 
+            if(hist[i]!=0):
+                #overall pixel intensities are added up until 'avg' length
+                overall=overall+hist[i]
+
+        overall_2 = 0
+        #same thing is done for the second half
+        for i in range(int(avg),int(len(hist))):
+
+            if (hist[i] != 0):
+                overall_2 = overall_2 + hist[i]
+
+        final_threshold_1=0
+        for i in range(int(avg)):
+            if(hist[i]!=0):
+                probability_1=hist[i]/overall
+                prob_dist_1=i*probability_1
+                final_threshold_1=prob_dist_1+final_threshold_1
+
+        final_threshold_2 = 0
+
+        for i in range(int(avg),int(len(hist))):
+            if(hist[i]!=0):
+                probPix=hist[i]/overall_2
+                prob_dist_2=i*probPix
+                final_threshold_2=prob_dist_2+final_threshold_2
+
+        threshold=(final_threshold_1+final_threshold_2)/2
+
+        
         return threshold
+
+    
 
     def binarize(self, image):
         """Comptues the binary image of the the input image based on histogram analysis and thresholding
