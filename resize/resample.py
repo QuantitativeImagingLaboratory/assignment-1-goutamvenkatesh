6e1329bd9987f3d6119ed80752f523afd1417060
goutamvenkatesh <goutam.venkatesh@gmail.com>
2017-10-02 21:52:18 -0500

    Update resample.py

--- resize/resample.py
+++ resize/resample.py
@@ -29,7 +29,7 @@ class resample:
         row,coloumn=image.shape
         new_row=row*fx
         new_column=coloumn*fy
-        output_image = numpy.zeros((int(new_row),int(new_column),3), numpy.uint8)
+        output_image = numpy.zeros((new_row,new_column,3), numpy.uint8)
         for i in range(int(new_row)-1):
             for j in range(int(new_column)-1):
                 row_value=int(math.floor(i/fx))
